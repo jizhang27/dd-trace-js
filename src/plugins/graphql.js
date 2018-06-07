41b0eb27e4de29358fb0e3e4f777c57083b5fcf9
rochdev <roch.devost@datadoghq.com>
2018-06-07 17:48:12 -0400

    wip

--- src/plugins/graphql.js
+++ src/plugins/graphql.js
@@ -2,53 +2,100 @@
 
 const shimmer = require('shimmer')
 
+let defaultFieldResolver = null
+
 function createWrapExecute (tracer, config) {
   return function wrapExecute (execute) {
-    return function executeWithTrace (schema) {
-      const fields = schema._queryType._fields
+    return function executeWithTrace () {
+      const args = normalizeArgs(arguments)
+      const schema = args.schema
+      const fieldResolver = args.fieldResolver || defaultFieldResolver
 
-      Object.keys(fields).forEach(key => {
-        const field = fields[key]
+      // console.log(args.document.definitions[0].selectionSet.selections)
 
-        if (fields && typeof field.resolve === 'function') {
-          const resolve = field.resolve
+      args.fieldResolver = wrapResolve(fieldResolver, tracer, config)
 
-          tracer.trace('graphql.query', span => {
-            span.addTags({
-              'service.name': config.service || (tracer._service && `${tracer._service}-graphql`) || 'graphql',
-              'resource.name': field.name
-            })
+      if (!schema._datadog_patched) {
+        wrapFields(schema._queryType._fields, tracer, config, [])
+        schema._datadog_patched = true
+      }
 
-            field.resolve = tracer.bind(wrapResolve(resolve, span))
-          })
-        }
-      })
-
-      return execute.apply(this, arguments)
+      return execute.call(this, args)
     }
   }
 }
 
-function wrapResolve (resolve, span) {
-  return function resolveWithTrace () {
-    try {
-      const result = resolve.apply(this, arguments)
+function wrapFields (fields, tracer, config) {
+  Object.keys(fields).forEach(key => {
+    const field = fields[key]
 
-      if (result && typeof result.then === 'function') {
-        return result
-          .then(value => {
-            span.finish()
-            return value
-          })
-          .catch(err => finishAndThrow(span, err))
-      } else {
-        span.finish()
-      }
-
-      return result
-    } catch (e) {
-      finishAndThrow(span, e)
+    if (typeof field.resolve === 'function') {
+      field.resolve = wrapResolve(field.resolve, tracer, config)
     }
+
+    if (field.type && field.type._fields) {
+      wrapFields(field.type._fields, tracer, config)
+    }
+  })
+}
+
+function wrapResolve (resolve, tracer, config) {
+  return function resolveWithTrace (source, args, contextValue, info) {
+    let result
+
+    tracer.trace('graphql.query', span => {
+      span.addTags({
+        'service.name': config.service || (tracer._service && `${tracer._service}-graphql`) || 'graphql',
+        'resource.name': getResource(info.path).join('.')
+      })
+
+      try {
+        result = resolve.apply(this, arguments)
+
+        if (result && typeof result.then === 'function') {
+          result = result
+            .then(value => {
+              span.finish()
+              return value
+            })
+            .catch(err => finishAndThrow(span, err))
+        } else {
+          span.finish()
+        }
+      } catch (e) {
+        finishAndThrow(span, e)
+      }
+    })
+
+    return result
+  }
+}
+
+function normalizeArgs (args) {
+  if (args.length === 1) {
+    return args[0]
+  }
+
+  return {
+    schema: args[0],
+    document: args[1],
+    rootValue: args[2],
+    contextValue: args[3],
+    variableValues: args[4],
+    operationName: args[5],
+    fieldResolver: args[6]
+  }
+}
+
+function getSelections (selectionSet) {
+  // path -> span
+}
+
+function getResource (path) {
+  if (path.prev) {
+    return getResource(path.prev).concat(path.key)
+  } else {
+    return [path.key]
   }
 }
 
@@ -75,9 +122,11 @@ module.exports = {
   file: 'execution/execute.js',
   versions: ['>=0.13.0 <1.0.0'],
   patch (execute, tracer, config) {
+    defaultFieldResolver = execute.defaultFieldResolver
     shimmer.wrap(execute, 'execute', createWrapExecute(tracer, config))
   },
   unpatch (execute) {
     shimmer.unwrap(execute, 'execute')
+    defaultFieldResolver = null
   }
 }
--- test/plugins/graphql.spec.js
+++ test/plugins/graphql.spec.js
@@ -1,19 +1,53 @@
 'use strict'
 
-const axios = require('axios')
-const getPort = require('get-port')
 const agent = require('./agent')
 
 describe('Plugin', () => {
   let plugin
   let context
   let graphql
+  let schema
 
   describe('express', () => {
     beforeEach(() => {
       plugin = require('../../src/plugins/graphql')
       graphql = require('graphql')
       context = require('../../src/platform').context({ experimental: { asyncHooks: false } })
+
+      schema = new graphql.GraphQLSchema({
+        query: new graphql.GraphQLObjectType({
+          name: 'RootQueryType',
+          fields: {
+            hello: {
+              type: graphql.GraphQLString,
+              args: {
+                name: {
+                  type: graphql.GraphQLString
+                }
+              },
+              resolve (obj, args) {
+                return args.name
+              }
+            },
+            human: {
+              type: new graphql.GraphQLObjectType({
+                name: 'Human',
+                fields: {
+                  name: {
+                    type: graphql.GraphQLString,
+                    resolve (obj, args) {
+                      return obj
+                    }
+                  }
+                }
+              }),
+              resolve (obj, args) {
+                return 'test'
+              }
+            }
+          }
+        })
+      })
     })
 
     afterEach(() => {
@@ -25,65 +59,153 @@ describe('Plugin', () => {
         return agent.load(plugin, 'graphql')
       })
 
-      it('should do automatic instrumentation on schema resolvers', done => {
-        // const schema = graphql.buildSchema(`
-        //   type Query {
-        //     hello: String
-        //     human(id: ID!): Human
-        //   }
-
-        //   type Human {
-        //     name: String
-        //   }
-        // `)
-
-        const schema = new graphql.GraphQLSchema({
-          query: new graphql.GraphQLObjectType({
-            name: 'RootQueryType',
-            fields: {
-              hello: {
-                type: graphql.GraphQLString,
-                resolve () {
-                  return 'world'
-                }
-              }
-            }
-          })
-        })
-
-        // console.log(schema._queryType._fields)
-
-        // const result = resolveField(
-        //   exeContext,
-        //   parentType,
-        //   sourceValue,
-        //   fieldNodes,
-        //   fieldPath,
-        // );
-
-        const query = `{ hello }`
-        // const query = `{ human(id: 1002) { name } }`
-
-        const rootValue = {
-          // hello: (obj, args, context) => {
-          //   // console.log(obj, args, context)
-          //   return 'Hello world!'
-          // },
-          human: {
-            resolve: () => {}
-          }
-        }
+      it('should instrument schema resolvers', done => {
+        const source = `{ hello(name: "world") }`
 
         agent
           .use(traces => {
             expect(traces[0][0]).to.have.property('service', 'test-graphql')
-            // expect(traces[0][0]).to.have.property('type', 'web')
+            expect(traces[0][0]).to.have.property('resource', 'hello')
+          }, 2)
+          .then(done)
+          .catch(done)
+
+        graphql.graphql(schema, source).catch(done)
+      })
+
+      it('should instrument nested field resolvers', done => {
+        const source = `{ human { name } }`
+
+        agent
+          .use(traces => {
+            expect(traces[0][0]).to.have.property('resource', 'human.name')
+          }, 2)
+          .then(done)
+          .catch(done)
+
+        graphql.graphql(schema, source).catch(done)
+      })
+
+      it('should instrument the default field resolver', done => {
+        const schema = graphql.buildSchema(`
+          type Query {
+            hello: String
+          }
+        `)
+
+        const source = `{ hello }`
+
+        agent
+          .use(traces => {
             expect(traces[0][0]).to.have.property('resource', 'hello')
           })
           .then(done)
           .catch(done)
 
-        graphql.graphql(schema, query, rootValue).catch(done)
+        graphql.graphql(schema, source, { hello: 'world' }).catch(done)
+      })
+
+      it('should instrument a custom field resolver', done => {
+        const schema = graphql.buildSchema(`
+          type Query {
+            hello: String
+          }
+        `)
+
+        const source = `{ hello }`
+
+        const rootValue = { hello: 'world' }
+
+        const fieldResolver = (source, args, contextValue, info) => {
+          return source[info.fieldName]
+        }
+
+        agent
+          .use(traces => {
+            expect(traces[0][0]).to.have.property('resource', 'hello')
+          })
+          .then(done)
+          .catch(done)
+
+        graphql.graphql({ schema, source, rootValue, fieldResolver }).catch(done)
+      })
+
+      it('should run the resolver in the trace context', done => {
+        const schema = graphql.buildSchema(`
+          type Query {
+            hello: String
+          }
+        `)
+
+        const source = `{ hello }`
+
+        const rootValue = { hello: 'world' }
+
+        const fieldResolver = (source, args, contextValue, info) => {
+          expect(context.get('current')).to.not.be.undefined
+          done()
+          return source[info.fieldName]
+        }
+
+        graphql.graphql({ schema, source, rootValue, fieldResolver }).catch(done)
+      })
+
+      it('should run nested resolvers in the parent context')
+
+      it('should handle exceptions', done => {
+        const error = new Error('test')
+
+        const schema = graphql.buildSchema(`
+          type Query {
+            hello: String
+          }
+        `)
+
+        const source = `{ hello }`
+
+        const fieldResolver = (source, args, contextValue, info) => {
+          throw error
+        }
+
+        agent
+          .use(traces => {
+            expect(traces[0][0]).to.have.property('error', 1)
+            expect(traces[0][0].meta).to.have.property('error.type', error.name)
+            expect(traces[0][0].meta).to.have.property('error.msg', error.message)
+            expect(traces[0][0].meta).to.have.property('error.stack', error.stack)
+          })
+          .then(done)
+          .catch(done)
+
+        graphql.graphql({ schema, source, fieldResolver }).catch(done)
+      })
+
+      it('should handle rejected promises', done => {
+        const error = new Error('test')
+
+        const schema = graphql.buildSchema(`
+          type Query {
+            hello: String
+          }
+        `)
+
+        const source = `{ hello }`
+
+        const fieldResolver = (source, args, contextValue, info) => {
+          return Promise.reject(error)
+        }
+
+        agent
+          .use(traces => {
+            expect(traces[0][0]).to.have.property('error', 1)
+            expect(traces[0][0].meta).to.have.property('error.type', error.name)
+            expect(traces[0][0].meta).to.have.property('error.msg', error.message)
+            expect(traces[0][0].meta).to.have.property('error.stack', error.stack)
+          })
+          .then(done)
+          .catch(done)
+
+        graphql.graphql({ schema, source, fieldResolver }).catch(done)
       })
     })
   })
